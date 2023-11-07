fa3150231ad9cbb84c914c91f11239abb82ab9d3
Serhii-Bondariev <xx.code4u@gmail.com>
2023-11-07 21:12:46 +0200

    Fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,3 @@
-// import React, { Component } from 'react';
-// import { v4 as uuidv4 } from 'uuid';
-// import ContactForm from './ContactForm/ContactForm';
-// import ContactList from './ContactList/ContactList';
-// import Filter from './Filter/Filter';
-// import styles from './App.module.css';
-
-// export class App extends Component {
-//   state = {
-//     contacts: [],
-//     filter: '',
-//   };
-
-//   addContact = (name, number) => {
-//     if (this.state.contacts.some(contact => contact.name === name)) {
-//       alert(`${name} is already in contacts.`);
-//       return;
-//     }
-
-//     const newContact = {
-//       id: uuidv4(),
-//       name,
-//       number,
-//     };
-
-//     this.setState(prevState => ({
-//       contacts: [...prevState.contacts, newContact],
-//     }));
-//   };
-
-//   deleteContact = id => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== id),
-//     }));
-//   };
-
-//   handleFilterChange = e => {
-//     this.setState({ filter: e.target.value });
-//   };
-
-//   render() {
-//     const { contacts, filter } = this.state;
-//     const filteredContacts = contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(filter.toLowerCase())
-//     );
-
-//     return (
-//       <div className={styles['app']}>
-//         <div className={styles['container']}>
-//           <div className={styles['app-wrapper']}>
-//             <h1 className={styles['phonebook-title']}>Phonebook</h1>
-//             <ContactForm onAddContact={this.addContact} />
-//             <h2 className={styles['contacts-title']}>Contacts</h2>
-//             <Filter filter={filter} onFilterChange={this.handleFilterChange} />
-//             <ContactList
-//               contacts={filteredContacts}
-//               onDeleteContact={this.deleteContact}
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
 import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import ContactForm from './ContactForm/ContactForm';
@@ -79,7 +12,6 @@ export class App extends Component {
   };
 
   componentDidMount() {
-    // При завантаженні компонента перевірте, чи є дані у localStorage
     const storedContacts = localStorage.getItem('contacts');
     if (storedContacts) {
       this.setState({ contacts: JSON.parse(storedContacts) });
@@ -87,9 +19,7 @@ export class App extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // Після оновлення стану перевірте, чи змінилася колекція контактів
     if (prevState.contacts !== this.state.contacts) {
-      // Якщо так, оновіть дані в localStorage
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,27 +1,3 @@
-// import React from 'react';
-
-// import styles from './ContactList.module.css';
-
-// export const ContactList = ({ contacts, onDeleteContact }) => (
-//   <div className={styles['contact-list-wrapper']}>
-//     <ul className={styles['contact-list']}>
-//       {contacts.map(contact => (
-//         <li className={styles['contact-item']} key={contact.id}>
-//           {contact.name}: {contact.number}
-//           <button
-//             className={styles['contact-btn']}
-//             onClick={() => onDeleteContact(contact.id)}
-//           >
-//             Delete contact
-//           </button>
-//         </li>
-//       ))}
-//     </ul>
-//   </div>
-// );
-
-// export default ContactList;
-
 import React from 'react';
 import styles from './ContactList.module.css';
 
